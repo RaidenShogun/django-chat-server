da72ba4c32ac37cbcefa67addee475a92bd9d04c
RaidenShogun <87703907+RaidenShogun@users.noreply.github.com>
2022-05-06 16:08:31 +0800

    Delete urls.py

--- urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import *
-# HTTP URL
-urlpatterns = [
-    path('',chatpage.as_view(),name="chat"),
-    path('message_ajax/<str:userid>',message_ajax,name='message_ajax')
-]
